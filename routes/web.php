5db16b2b9702e491cd20b345b618589a4702866d
AsifRp <asifrp6@gmail.com>
2024-08-12 00:10:55 +0600

    master index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/index', [BackendController::class, 'index'])->name('index');
+
